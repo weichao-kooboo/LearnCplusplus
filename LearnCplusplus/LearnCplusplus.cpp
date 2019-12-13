a975e406343708a36339480d4123f13c386c012f
weichao <weichao@kooboo.net>
2019-12-13 18:25:25 +0800

    add files

--- LearnCplusplus/LearnCplusplus.cpp
+++ LearnCplusplus/LearnCplusplus.cpp
@@ -4,9 +4,31 @@
 #include "pch.h"
 #include <iostream>
 
+//ref:
+//https://www.cnblogs.com/cthon/p/9166715.html,
+//https://www.cnblogs.com/cthon/p/9178701.html,
+//https://segmentfault.com/a/1190000018096022
+
 int main()
 {
     std::cout << "Hello World!\n"; 
+
+	const int a = 1;
+	// exception a为const，常数型数
+	//a = 2;
+	int const b = 2;
+	// exception	b为const，常数型数
+	//b = 3;
+
+    const int *c = &a;
+	// exception	c为const,指向长整型数的指针（所指向的内存数据不能修改，但本身可以修改）
+	//*c = 4;
+
+	int m = 1;
+	int n = 2;
+	int* const d = &m;
+	// exception	int*为const，常量指针（指针变量不能被修改，但是它所指向内存空间可以被修改）
+	//d = &n;
 }
 
 // 运行程序: Ctrl + F5 或调试 >“开始执行(不调试)”菜单
